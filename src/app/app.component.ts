a1785ed71e993b8c3b8468ee53aaa38b6f1a457a
uzumakiwesley <uzumakiwesley58@gmail.com>
2022-10-14 16:13:00 -0300

    commit databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'data-binding';
+/**
+ * String Interpolation: utilizar valores que propriedades
+ * possuem para mostra-los para o usuário
+ */
+
+/**
+ * property bindings utilizar valores de propriedades
+ * atribui-los a atributos HTML
+ */
+
+/**
+ * event binding: utilizado para ouvir eventos do DOM
+ * dentro do angulat
+ */
+
+/**
+ * two way data binding
+ */
+
+  title = 'Angular é muito bom!';
+  n1: number = 0
+  n2: number = 0
+  tipoInput: string = 'password'
+  somar(x: number, y: number) {
+    return x + y
+  }
+
+mudarTipoDoInput(): void {
+  if (this.tipoInput == 'password') {
+    this.tipoInput = 'text'
+  } else {
+    this.tipoInput = 'password'
+  }
+}
+
 }
